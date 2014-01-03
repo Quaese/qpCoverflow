794cbf4fabf340b182e3c1a9861df699434ef851
Quaese <quaese@gmx.net>
2014-01-03 18:53:42 +0100

    ID Handling fuer Module eingefuegt

--- output/frontend.inc.php
+++ output/frontend.inc.php
@@ -1,12 +1,20 @@
-<div id="coverflow"></div>
-
 <?php
+// ID Variable
+$strCoverflowId = "coverflow" . ((isset($REXVALUE[1]) && $REXVALUE[1]!="")? "_".$REXVALUE[1] : "");
+
+echo('<div id="'.$strCoverflowId.'"></div>');
 echo('<link href="' . $REX['HTDOCS_PATH'] . 'files/addons/qp_coverflow/qp_coverflow.css" rel="stylesheet" type="text/css">');
 echo('<script src="' . $REX['HTDOCS_PATH'] . 'files/addons/qp_coverflow/qp_coverflow02.jquery.js" type="text/javascript"></script>');
 ?>
 
 <script type="text/javascript">
-$(function(){
-    $('#coverflow').qpCoverflow();
-});
+if(jQuery){
+    (function($){
+		$(function(){
+			if($.custom && $.custom.qpCoverflow){
+		    	$('#<?php echo($strCoverflowId); ?>').qpCoverflow();
+			}
+		});
+    })(jQuery);
+}
 </script>
